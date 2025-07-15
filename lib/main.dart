4b56aaebacbb9d2d5f21838fc096ba76a5cc1d19
Xiaotong Guo <guo00156@algonquinlive.com>
2025-07-15 11:44:22 -0400

    WEEK8 DATABASE version 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shopping List with DB',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      title: 'Shopping List with Database',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor:Colors.deepPurple),),
       home: ShoppingListPage(database: database),
     );
   }
@@ -57,7 +57,7 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
   Future<void> _addItem() async {
     final name = _itemController.text.trim();
     final quantity = int.tryParse(_quantityController.text.trim()) ?? 0;
-
+    // if its not empty add it to the _items list, and clear the textfield.
     if (name.isNotEmpty && quantity > 0) {
       final newItem = Item(name: name, quantity: quantity);
       await widget.database.itemDao.insertItem(newItem);
@@ -72,6 +72,7 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
     await _loadItemsFromDb();
   }
 
+//Prompt the alertDialog, if click "yes"  setState to delete, if click "no"
   void _confirmDelete(Item item) {
     showDialog(
       context: context,
@@ -102,44 +103,56 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Shopping List with DB')),
+      appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Shopping List with DB')),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
               children: [
                 Expanded(
                   child: TextField(
                     controller: _itemController,
-                    decoration: const InputDecoration(hintText: 'Item name'),
+                    decoration: const InputDecoration(
+                        hintText: 'Type the item here', border: OutlineInputBorder()),
                   ),
                 ),
-                const SizedBox(width: 10),
-                SizedBox(
-                  width: 80,
+                const SizedBox(width: 2),
+                Expanded(
                   child: TextField(
                     controller: _quantityController,
-                    decoration: const InputDecoration(hintText: 'Qty'),
+                    decoration: const InputDecoration(
+                        hintText: 'Type the quantity here',border:OutlineInputBorder()),
                     keyboardType: TextInputType.number,
                   ),
                 ),
                 const SizedBox(width: 10),
-                ElevatedButton(onPressed: _addItem, child: const Text('Add')),
+                ElevatedButton(
+                    onPressed: _addItem,
+                    child: const Text('Click here')),
               ],
             ),
             const SizedBox(height: 20),
             Expanded(
               child: _items.isEmpty
-                  ? const Center(child: Text('No items'))
+                  ? const Center(child: Text('There are no items in the list'))
                   : ListView.builder(
                 itemCount: _items.length,
                 itemBuilder: (context, index) {
                   final item = _items[index];
-                  return ListTile(
-                    title: Text(item.name),
-                    subtitle: Text('Quantity: ${item.quantity}'),
+                  return GestureDetector(
                     onLongPress: () => _confirmDelete(item),
+                    child:Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:[
+                        Text("${index + 1}. "),
+                        Text(item.name),
+                        Text(" Qty: ${item.quantity}"),
+                      ]
+                    )
                   );
                 },
               ),
